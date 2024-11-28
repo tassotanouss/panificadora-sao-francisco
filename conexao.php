4572195412e8fc16ff3a97605207128fe9534e67
Matheus Ferreira Amaral <168740667+mateto2@users.noreply.github.com>
2024-11-28 19:44:02 -0300

    Create conexao.php

---
+++ conexao.php
@@ -0,0 +1,13 @@
+<?php
+$host = "localhost";
+$username = "root";
+$password = "";
+$dbname = "carrinho_compras";
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Erro ao conectar com o banco de dados: " . $e->getMessage());
+}
+?>
